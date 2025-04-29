4dacde9f950afd2120ec405a9df8f8d0a7e4eb61
Álvaro <alvarochuan@gmail.com>
2025-04-29 14:55:59 +0200

    Build funcionando

--- Assets/Scripts/StartScreen/InitialUIManager.cs
+++ Assets/Scripts/StartScreen/InitialUIManager.cs
@@ -4,7 +4,6 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Numerics;
 using TMPro;
-using UnityEditor.UIElements;
 using UnityEngine;
 using UnityEngine.Rendering;
 using UnityEngine.SceneManagement;
